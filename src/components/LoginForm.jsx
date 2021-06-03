394dfcd8ba9fd634fdf498744db2a46d67ccf518
Sajarin M <sajarinm@yahoo.com>
2021-06-03 19:41:22 +0100

    removed access to signup route if user is logged in

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,99 +2,102 @@ import React, { useState } from "react";
 import { Redirect, useLocation } from "react-router-dom";
 import Joi from "joi-browser";
 import useForm from "./common/Form/useForm";
-import StartupForm from "./common/StartupForm/StartupForm";
 import authService from "./../services/authService";
+import { UserContext } from "../contexts/userContext";
+import StartupForm from "./common/StartupForm/StartupForm";
 
 const LoginForm = () => {
-	const [data, setData] = useState({ username: "", password: "" });
-	const [errors, setErrors] = useState({});
+    const { user } = useContext(UserContext);
 
-	const location = useLocation();
+    const [data, setData] = useState({ username: "", password: "" });
+    const [errors, setErrors] = useState({});
 
-	const {
-		handleSubmit,
-		renderTextInput,
-		renderSubmitButton,
-		renderResetButton,
-	} = useForm({
-		data,
-		setData,
-		errors,
-		setErrors,
-		doSubmit,
-		requiresValidation: true,
-		schema: {
-			username: Joi.string().min(3).max(255).email().required(),
-			password: Joi.string().min(5).max(255).required(),
-		},
-	});
+    const location = useLocation();
 
-	const formData = {
-		heading: "Login",
-		description:
-			"Get Personalized access to your Exams, Results and Recommendations",
-		link: {
-			title: "New to Online examination? Create an account",
-			to: "/signup",
-		},
-	};
+    const {
+        handleSubmit,
+        renderTextInput,
+        renderSubmitButton,
+        renderResetButton,
+    } = useForm({
+        data,
+        setData,
+        errors,
+        setErrors,
+        doSubmit,
+        requiresValidation: true,
+        schema: {
+            username: Joi.string().min(3).max(255).email().required(),
+            password: Joi.string().min(5).max(255).required(),
+        },
+    });
 
-	async function doSubmit() {
-		try {
-			await authService.login(data.username, data.password);
-			const { state } = location;
-			window.location = state ? state.from.pathname : "/";
-		} catch (error) {
-			if (error.response && error.response.status === 400) {
-				const errors = {};
-				errors.server = error.response.data;
-				setErrors(errors);
-			}
-		}
-	}
+    const formData = {
+        heading: "Login",
+        description:
+            "Get Personalized access to your Exams, Results and Recommendations",
+        link: {
+            title: "New to Online examination? Create an account",
+            to: "/signup",
+        },
+    };
 
-	if (authService.getCurrentUser()) return <Redirect to="/exams" />;
-	const { server: serverError } = errors;
+    async function doSubmit() {
+        try {
+            await authService.login(data.username, data.password);
+            const { state } = location;
+            window.location = state ? state.from.pathname : "/";
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                const errors = {};
+                errors.server = error.response.data;
+                setErrors(errors);
+            }
+        }
+    }
 
-	return (
-		<StartupForm formData={formData}>
-			<form className="form" onSubmit={handleSubmit}>
-				<div
-					className={`form__error form__error--server ${
-						serverError ? "" : "u-hidden"
-					}`}
-				>
-					{serverError || "error"}
-				</div>
-				{renderTextInput({
-					name: "username",
-					label: "Username",
-					placeholder: "Username",
-					className: "welcome-form__input",
-					formGroupClassName: "welcome-form__group",
-					formLabelClassName: "welcome-form__label",
-					autoFocus: true,
-				})}
-				{renderTextInput({
-					name: "password",
-					label: "Password",
-					className: "welcome-form__input",
-					formLabelClassName: "welcome-form__label",
-					formGroupClassName: "welcome-form__group",
-					placeholder: "Password",
-					type: "password",
-				})}
-				<div className="form__button-group">
-					{renderSubmitButton({
-						className: "welcome__btn welcome__btn--green",
-					})}
-					{renderResetButton({
-						className: "welcome__btn welcome__btn--green",
-					})}
-				</div>
-			</form>
-		</StartupForm>
-	);
+    if (user) return <Redirect to="/exams" />;
+    const { server: serverError } = errors;
+
+    return (
+        <StartupForm formData={formData}>
+            <form className="form" onSubmit={handleSubmit}>
+                <div
+                    className={`form__error form__error--server ${
+                        serverError ? "" : "u-hidden"
+                    }`}
+                >
+                    {serverError || "error"}
+                </div>
+                {renderTextInput({
+                    name: "username",
+                    label: "Username",
+                    placeholder: "Username",
+                    className: "welcome-form__input",
+                    formGroupClassName: "welcome-form__group",
+                    formLabelClassName: "welcome-form__label",
+                    autoFocus: true,
+                })}
+                {renderTextInput({
+                    name: "password",
+                    label: "Password",
+                    className: "welcome-form__input",
+                    formLabelClassName: "welcome-form__label",
+                    formGroupClassName: "welcome-form__group",
+                    placeholder: "Password",
+                    type: "password",
+                })}
+                <div className="form__button-group">
+                    {renderSubmitButton({
+                        className: "welcome__btn welcome__btn--green",
+                    })}
+                    {renderResetButton({
+                        className: "welcome__btn welcome__btn--green",
+                    })}
+                </div>
+            </form>
+        </StartupForm>
+    );
 };
 
 export default LoginForm;
--- src/components/common/Sidebar/Sidebar.jsx
+++ src/components/common/Sidebar/Sidebar.jsx
@@ -8,86 +8,86 @@ import profile from "../../../assets/icons/profile.svg";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-	const { user } = useContext(UserContext);
-	const { writeMode } = useContext(ExamContext);
+    const { user } = useContext(UserContext);
+    const { writeMode } = useContext(ExamContext);
 
-	const navlinks = [
-		{
-			label: "Exams",
-			route: "/exams",
-			icon: { name: "document-edit" },
-		},
-		{
-			label: "Students",
-			route: "/students",
-			renderCondition: Boolean(user && user.isTeacher),
-			icon: { name: "graduation-cap" },
-		},
-		{
-			label: "Teachers",
-			route: "/teachers",
-			renderCondition: Boolean(user && !user.isTeacher),
-			icon: { name: "group" },
-		},
-		{
-			label: "Requests",
-			route: "/requests",
-			icon: { name: "bubble" },
-		},
-		{
-			label: "Profile",
-			route: "/profile",
-			icon: { name: "user-solid-circle" },
-		},
-		{
-			label: "Logout",
-			route: "/logout",
-			icon: { name: "stand-by" },
-		},
-	];
+    const navlinks = [
+        {
+            label: "Exams",
+            route: "/exams",
+            icon: { name: "document-edit" },
+        },
+        {
+            label: "Students",
+            route: "/students",
+            renderCondition: Boolean(user && user.isTeacher),
+            icon: { name: "graduation-cap" },
+        },
+        {
+            label: "Teachers",
+            route: "/teachers",
+            renderCondition: Boolean(user && !user.isTeacher),
+            icon: { name: "group" },
+        },
+        {
+            label: "Requests",
+            route: "/requests",
+            icon: { name: "bubble" },
+        },
+        {
+            label: "Profile",
+            route: "/profile",
+            icon: { name: "user-solid-circle" },
+        },
+        {
+            label: "Logout",
+            route: "/logout",
+            icon: { name: "stand-by" },
+        },
+    ];
 
-	return (
-		<div
-			className={`sidebar ${!user || writeMode ? "sidebar-hidden" : ""}`}
-		>
-			<div className="logo-box">
-				<img src={logo} alt="trillo logo" className="logo" />
-				<div className="text-box">
-					<p>online </p>
-					<p>examination</p>
-				</div>
-			</div>
-			<div className="sidebar__profile">
-				<img src={profile} alt="profile" className="profile__img" />
-				<div className="profile__greet">
-					<h2>{user && user.name}</h2>
-				</div>
-			</div>
-			<ul className="sidebar-nav">
-				{navlinks.map((navlink) => {
-					const { route, label, icon, renderCondition } = navlink;
-					if (renderCondition === false) return null;
-					return (
-						<li key={route} className="sidebar-nav__item">
-							<NavLink
-								to={route}
-								className="sidebar-nav__link"
-								activeClassName="sidebar-nav__link--active"
-							>
-								{icon && (
-									<Icon
-										className="sidebar-nav__icon"
-										{...icon}
-									/>
-								)}
-								<span>{label}</span>
-							</NavLink>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+    return (
+        <div
+            className={`sidebar ${!user || writeMode ? "sidebar-hidden" : ""}`}
+        >
+            <div className="logo-box">
+                <img src={logo} alt="trillo logo" className="logo" />
+                <div className="text-box">
+                    <p>online </p>
+                    <p>examination</p>
+                </div>
+            </div>
+            <div className="sidebar__profile">
+                <img src={profile} alt="profile" className="profile__img" />
+                <div className="profile__greet">
+                    <h2>{user && user.name}</h2>
+                </div>
+            </div>
+            <ul className="sidebar-nav">
+                {navlinks.map((navlink) => {
+                    const { route, label, icon, renderCondition } = navlink;
+                    if (renderCondition === false) return null;
+                    return (
+                        <li key={route} className="sidebar-nav__item">
+                            <NavLink
+                                to={route}
+                                className="sidebar-nav__link"
+                                activeClassName="sidebar-nav__link--active"
+                            >
+                                {icon && (
+                                    <Icon
+                                        className="sidebar-nav__icon"
+                                        {...icon}
+                                    />
+                                )}
+                                <span>{label}</span>
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default Sidebar;
